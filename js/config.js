da50d50b302dfe5defbe8603af6da5f6e7d172f2
Hudhayfah7 <huddy97k@gmail.com>
2025-11-04 02:54:30 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_ZbpP2kG56', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6vs2onb8krj56of4cjgfermf79', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_1ehjNVGSV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4npacevhrblvc309dv157k43fi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1', // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
